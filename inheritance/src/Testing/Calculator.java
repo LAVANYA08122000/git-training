1a0a4f75e809b02510deb513705de5562347c6fb
lavanya08122000 <lavanyad698@gmail.com>
2022-08-10 12:31:48 +0530

    commited a calculator file

---
+++ inheritance/src/Testing/Calculator.java
@@ -0,0 +1,34 @@
+package Testing;
+
+import java.util.Scanner;
+public class Calculator {
+	/*public int fact(int a) {
+		int f=1;
+		for(int i=1;i<=a;i++)
+			f=f*i;
+		return f;
+	}*/
+	
+	public boolean prime(int a) {
+		if(a==0||a==1)
+			return false;
+		else {
+		for(int i=2;i<=a/2;i++) {
+			if(a%2==0) {
+				return false;
+			}
+		}
+		}
+		return true;
+	}
+	public static  int add(int a,int b) {
+		return a+b;
+	}
+	/*public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		Calculator c=new Calculator();
+		int fact=c.fact(a);
+		System.out.println(fact);
+	}*/
+}
